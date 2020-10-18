9c609381563340b9bbb60b9a62a00ce152d7748a
akash-akya <akashh246@gmail.com>
2020-10-18 21:37:32 +0530

    In the beginning there was darkness

---
+++ lib/eips.ex
@@ -0,0 +1,18 @@
+defmodule Eips do
+  @moduledoc """
+  Documentation for Eips.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eips.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eips_test.exs
@@ -0,0 +1,8 @@
+defmodule EipsTest do
+  use ExUnit.Case
+  doctest Eips
+
+  test "greets the world" do
+    assert Eips.hello() == :world
+  end
+end
