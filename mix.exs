2a91287402fa55b450b6ed9b314961a7cf404410
akash-akya <akashh246@gmail.com>
2020-11-19 23:17:19 +0530

    Update elixir requirement

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Vix.MixProject do
     [
       app: :vix,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:elixir_make] ++ Mix.compilers(),
