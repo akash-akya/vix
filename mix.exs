e6c2fa2bb9e228dfba0c4eb7a900359d3280f1f7
akash-akya <akashh246@gmail.com>
2021-05-04 21:04:04 +0530

    Bump to v0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vix.MixProject do
   def project do
     [
       app: :vix,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
