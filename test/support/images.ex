918196c4b34afb1a03c35542ced1bd9cfddfac95
akash-akya <akashh246@gmail.com>
2022-08-14 17:33:07 +0530

    Fix endianness test

--- test/support/images.ex
+++ test/support/images.ex
@@ -10,4 +10,14 @@ defmodule Vix.Support.Images do
   def img_path(name) do
     Path.join(@images_path, name)
   end
+
+  def assert_images_equal(a, b) do
+    with {:ok, img} <- Vix.Vips.Operation.relational(a, b, :VIPS_OPERATION_RELATIONAL_EQUAL) do
+      {min, _additional_output} = Vix.Vips.Operation.min!(img, size: 1)
+      assert min == 255.0, "Images are not equal"
+    else
+      {:error, reason} ->
+        flunk("Failed to compare images, error: #{inspect(reason)}")
+    end
+  end
 end
