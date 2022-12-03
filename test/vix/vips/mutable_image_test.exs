e9ac600d44181f78a8fd415436ea759b3357ab50
Kip Cole <kipcole9@gmail.com>
2022-12-03 06:42:16 +0000

    Add MutableImage attribute access Adds: * MutableImage.width/1 * MutableImage.height/1 * MutableImage.bands/1 * MutableImage.has_alpha?/1

--- test/vix/vips/mutable_image_test.exs
+++ test/vix/vips/mutable_image_test.exs
@@ -51,4 +51,14 @@ defmodule Vix.Vips.MutableImageTest do
 
     assert {:ok, 0} == Image.header_value(new_img, "orientation")
   end
+
+  test "introspection" do
+    {:ok, i} = Vix.Vips.Image.new_from_file(img_path("puppies.jpg"))
+
+    assert {:ok, {_, 518}} = Vix.Vips.Image.mutate(i, fn m -> Vix.Vips.MutableImage.width(m) end)
+    assert {:ok, {_, 389}} = Vix.Vips.Image.mutate(i, fn m -> Vix.Vips.MutableImage.height(m) end)
+    assert {:ok, {_, 3}} = Vix.Vips.Image.mutate(i, fn m -> Vix.Vips.MutableImage.bands(m) end)
+    assert {:ok, {_, false}} = Vix.Vips.Image.mutate(i, fn m -> Vix.Vips.MutableImage.has_alpha?(m) end)
+
+  end
 end
