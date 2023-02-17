4b1c920bff80ce3b5005bd29d840059f2f7c220a
akash-akya <akashh246@gmail.com>
2023-02-17 22:30:10 +0530

    Add checksum file for testing

---
+++ checksum.exs
@@ -0,0 +1,8 @@
+%{
+  "vix-nif-2.16-aarch64-apple-darwin-0.16.1-rc1.tar.gz" => "sha256:0447d395f8b8907ac28f5604ec3eb6b19a1462a9e6cad741b45cfc8e21a3b386",
+  "vix-nif-2.16-aarch64-linux-gnu-0.16.1-rc1.tar.gz" => "sha256:46e07514c1693f77c681ac8ac92f213fe1a63587a928091054c24b1c60a92760",
+  "vix-nif-2.16-aarch64-linux-musl-0.16.1-rc1.tar.gz" => "sha256:f6fdc066709e607b903145b7ce48f81267f93ca12d4ed0c4e0d3c334af5cf63c",
+  "vix-nif-2.16-x86_64-apple-darwin-0.16.1-rc1.tar.gz" => "sha256:f9d50269576c254553133afe0f40cdba19d63111885908eaced581dc7f9bf2a5",
+  "vix-nif-2.16-x86_64-linux-gnu-0.16.1-rc1.tar.gz" => "sha256:b9e93a5c87c9e294414795472e99f94be8331ddee94710fa27dfc72a037af129",
+  "vix-nif-2.16-x86_64-linux-musl-0.16.1-rc1.tar.gz" => "sha256:b800aa910f239cfaa18a4ff307d95a6f49531b92037cdecdbfdf245b6122cbe7",
+}
