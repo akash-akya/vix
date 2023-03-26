1de5a0c9e88a9cc86687468515ef2efe8a70143a
Minku Lee <public@premi.st>
2023-03-26 22:30:08 +0900

    Support 1.x castore dependency version
    Co-authored-by: Akash Hiremath <akashh246@gmail.com>

--- mix.exs
+++ mix.exs
@@ -119,7 +119,7 @@ defmodule Vix.MixProject do
       [
         {:elixir_make, "~> 0.8 or ~> 0.7.3", runtime: false},
         {:cc_precompiler, "~> 0.2 or ~> 0.1.4", runtime: false},
-        {:castore, "~> 0.1"},
+        {:castore, "~> 1.0 or ~> 0.1"},
 
         # development & test
         {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
