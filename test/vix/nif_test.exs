6739e84d1ca7e01ee91030fb3fea87c1598c23cb
Håvard Lindset <havard@givn.no>
2026-05-22 13:35:21 +0200

    Add pipe owner exit regression test

--- test/vix/nif_test.exs
+++ test/vix/nif_test.exs
@@ -20,4 +20,56 @@ defmodule Vix.NifTest do
 
     assert IO.iodata_length(binary) == width * height * bands
   end
+
+  test "target read fd closes when owner exits" do
+    parent = self()
+
+    owner =
+      spawn(fn ->
+        {:ok, {read_fd, _target}} = Nif.nif_target_new()
+        send(parent, {:target_read_fd, read_fd})
+
+        receive do
+          :stop -> :ok
+        end
+      end)
+
+    assert_receive {:target_read_fd, read_fd}, 1_000
+
+    ref = Process.monitor(owner)
+    send(owner, :stop)
+
+    assert_receive {:DOWN, ^ref, :process, ^owner, :normal}
+    assert {:error, "Bad file descriptor"} = Nif.nif_read(read_fd, 1)
+  end
+
+  test "target read fd closes when owner exits after waiting for input" do
+    parent = self()
+
+    owner =
+      spawn(fn ->
+        {:ok, {read_fd, _target}} = Nif.nif_target_new()
+        send(parent, {:target_read_fd, read_fd})
+
+        receive do
+          :select ->
+            send(parent, {:select_result, Nif.nif_read(read_fd, 1)})
+        end
+
+        receive do
+          :stop -> :ok
+        end
+      end)
+
+    assert_receive {:target_read_fd, read_fd}, 1_000
+
+    send(owner, :select)
+    assert_receive {:select_result, {:error, :eagain}}, 1_000
+
+    ref = Process.monitor(owner)
+    send(owner, :stop)
+
+    assert_receive {:DOWN, ^ref, :process, ^owner, :normal}
+    assert {:error, "Bad file descriptor"} = Nif.nif_read(read_fd, 1)
+  end
 end
